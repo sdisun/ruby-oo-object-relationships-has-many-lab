346acc633f200c37e6ba5f0349d58e9a09b18203
Neil <zooming.in@hotmail.com>
2021-08-08 17:32:30 +1000

    updates

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author
+    attr_accessor :author
+    attr_reader :name
+
+    def initialize(author)
+        @author = author
+        @name = author
+    end
+
+    def posts
+        Post.all
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def add_post_by_title(title)
+        new_post = Post.new(title)
+        new_post.author = self
+    end
+
+    def self.post_count
+        Post.all.length
+    end
+
+end
